a3736ed4b7caa9744fe7145b7919b27bfd1a5798
Andrew <andrewrfindlay@ntlworld.com>
2019-06-21 11:56:28 +0100

    Date and time showing over photo

--- imageManager.py
+++ imageManager.py
@@ -1,8 +1,14 @@
 import pygame
 from pygame.locals import *
 
-class Photo:
-     
+import re
+
+class Photo:     
+
+    # File path is written like "./Images/2019-02-28/taskboardImage_12-58-25.jpg"
+    appendedString = "taskboardImage_"
+
+
     def __init__(self, filePath):
         self.image = pygame.image.load(filePath)
         # Save scaled version of image
@@ -12,7 +18,23 @@ class Photo:
         print(self.image.get_size())
         self.xpos = 0
         self.ypos = 0
+        # # Split up file name from directories
+        # processText = filePath.split("/")
+        # # Get date value from 2nd directory
+        # self.dateText = processText[1]
+        # # Remove appended string from before file name
+        # processText = processText[2].split
+        
+        # processText = processText[1]
+        # processText = processText.split("_")
+        # self.timeText = processText[1]
 
+        self.dateText = re.search("Images/(.*)/", filePath).group(1)
+        self.timeText = re.search("taskboardImage_(.*).jpg", filePath).group(1)
+        print(self.dateText)
+        print(self.timeText)
+
+    
     def draw(self, screen):
         screen.blit(self.scaledImage, (self.xpos, self.ypos))
 
--- showImages.py
+++ showImages.py
@@ -9,6 +9,10 @@ pygame.init()
 screen = pygame.display.set_mode((width, height), HWSURFACE|DOUBLEBUF|RESIZABLE)
 pygame.display.set_caption('Taskboard Timeline')
 
+# Initalise text
+pygame.font.init()
+myfont = pygame.font.SysFont('Helvetica', 30)
+
 # Initialise photos
 photo0 = Photo("./Images/2019-02-28/taskboardImage_12-58-25.jpg")
 photo1 = Photo("./Images/2019-02-28/taskboardImage_13-00-10.jpg")
@@ -22,6 +26,12 @@ def updateImage():
     # Only update when image is changed
     IMAGES[currentImage].resize(width, height)
     IMAGES[currentImage].draw(screen)
+    # Write text
+    dateText = myfont.render(IMAGES[currentImage].dateText, False, (0, 0, 0))
+    timeText = myfont.render(IMAGES[currentImage].timeText, False, (0, 0, 0))
+    screen.blit(dateText,(0, 0))
+    screen.blit(timeText,(0, 30))
+    
     # Update all changed screen elements
     pygame.display.update()
 
